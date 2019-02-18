c4946ce68d7393ec34b01d502aeabb4ba0e59357
Dave Ramirez <ramirezd42@gmail.com>
2019-02-17 20:10:31 -0500

    pull out some functions for funzies

--- ch2/src/main.rs
+++ ch2/src/main.rs
@@ -1,23 +1,21 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use std::num;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = seed_solution();
     let mut num_guesses = 0;
 
     loop {
         num_guesses += 1;
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
 
+        let guess = next_guess();
         println!("You guess: {}", guess);
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match parse_answer(guess) {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -31,4 +29,20 @@ fn main() {
             }
         }
     }
+}
+
+fn seed_solution() -> i32 {
+    rand::thread_rng().gen_range(1, 101)
+}
+
+fn parse_answer(input: String) -> Result<i32, num::ParseIntError> {
+    input.trim().parse()
+}
+
+fn next_guess() -> String {
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    guess
 }
